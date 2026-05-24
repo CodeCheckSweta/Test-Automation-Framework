334ddd2744443c453e403b8fd1abc37d6be19ae6
Sweta Singh <sweta.singh0322@gmail.com>
2026-05-24 17:05:29 +0530

    fixed path

--- src/test/java/com/utility/BrowserUtility.java
+++ src/test/java/com/utility/BrowserUtility.java
@@ -104,7 +104,7 @@ public abstract class BrowserUtility {
 		Date date = new Date();
 		SimpleDateFormat format = new SimpleDateFormat("HH-mm-ss");
 		String timestamp = format.format(date);
-		String path = System.getProperty("user.dir")+"/screenshots/"+ name +" - " + timestamp +".png";
+		String path = "./screenshots/"+ name +" - " + timestamp +".png";
 		File screenshotData = screenshot.getScreenshotAs(OutputType.FILE);
 		File screenshotFile = new File(path);
 				
